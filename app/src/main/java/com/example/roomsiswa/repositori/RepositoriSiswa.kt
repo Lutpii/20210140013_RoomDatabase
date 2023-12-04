9b84c7c6c7be37d70891210ba208559340808eef
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2023-12-05 02:03:29 +0700

    menambahkan fun getAllsiswa

--- app/src/main/java/com/example/roomsiswa/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomsiswa/repositori/RepositoriSiswa.kt
@@ -1,4 +1,8 @@
 package com.example.roomsiswa.repositori
 
+import com.example.roomsiswa.data.Siswa
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
 }
