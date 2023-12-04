e75697e2eb6b08f8ddf67345e01391ec64c6ea07
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2023-12-05 01:18:34 +0700

    Menambahkan method Update

--- app/src/main/java/com/example/roomsiswa/data/SiswaDao.kt
+++ app/src/main/java/com/example/roomsiswa/data/SiswaDao.kt
@@ -3,10 +3,14 @@ package com.example.roomsiswa.data
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Update
 
 @Dao
 interface SiswaDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    @Update
+    suspend fun update(siswa: Siswa)
 
 }
