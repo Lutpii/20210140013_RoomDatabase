2c9f8c625688c50173406a1bd9cb0b9c56a95be0
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2023-12-05 01:17:24 +0700

    Menambahkan method Insert

--- app/src/main/java/com/example/roomsiswa/data/SiswaDao.kt
+++ app/src/main/java/com/example/roomsiswa/data/SiswaDao.kt
@@ -1,7 +1,12 @@
 package com.example.roomsiswa.data
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 
 @Dao
 interface SiswaDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+
 }
