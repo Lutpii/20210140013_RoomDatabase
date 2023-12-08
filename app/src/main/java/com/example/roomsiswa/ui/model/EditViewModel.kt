1dea1cbb14468a138f2ff42772d3e07d93a1a6d3
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2023-12-08 09:31:55 +0700

    mendeklarasikan variable dan menambahkan init

--- app/src/main/java/com/example/roomsiswa/ui/model/EditViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/ui/model/EditViewModel.kt
@@ -1,11 +1,32 @@
 package com.example.roomsiswa.ui.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.roomsiswa.repositori.RepositoriSiswa
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class EditViewModel (
     savedStateHandle: SavedStateHandle,
     private val repositoriSiswa: RepositoriSiswa
 ) : ViewModel() {
+
+    var siswaUiState by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val itemId: Int = checkNotNull(savedStateHandle[ItemEditDestination.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            siswaUiState = repositoriSiswa.getSiswaStream(itemId)
+                .filterNotNull()
+                .first()
+                .toUiStateSiswa(true)
+        }
+    }
 }
